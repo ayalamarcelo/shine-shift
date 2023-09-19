860270222da0b5673edad2e44d5b9fd6671d22c3
Marcelo <marcelorubenayala@outlook.com>
2023-09-19 20:09:43 -0300

    Latest updates

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-let icon = document.getElementById("icon");
+var icon = document.getElementById("icon");
+var logo = document.getElementById("logo");
+var picture = document.getElementById("picture");
 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
     icon.src = "./images/toggle-off.svg";
+    logo.src = "./images/white-logo.png";
+    picture.src = "./images/img-white.png";
   } else {
     icon.src = "./images/toggle-on.svg";
+    logo.src = "./images/dark-logo.png";
+    picture.src = "./images/img-black.png";
   }
 }
