f03287b246a52ee5255a2eb9aa8b5e168d769428
Marcelo <marcelorubenayala@outlook.com>
2023-09-27 22:09:41 -0300

    Latest updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var icon = document.getElementById("icon");
-var logo = document.getElementById("logo");
-var picture = document.getElementById("picture");
+const icon = document.getElementById("icon");
+const logo = document.getElementById("logo");
+const picture = document.getElementById("picture");
+const btnMenu = document.getElementById("btn-menu");
 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
@@ -8,15 +9,17 @@ icon.onclick = function () {
     icon.src = "./src/toggle-off.svg";
     logo.src = "./src/white-logo.png";
     picture.src = "./src/img-white.png";
+    btnMenu.src = "./src/menu-white.svg"
   } else {
     icon.src = "./src/toggle-on.svg";
     logo.src = "./src/dark-logo.png";
     picture.src = "./src/img-black.png";
+    btnMenu.src = "./src/menu-black.svg";
   }
 }
 
-document.getElementById("btn-menu").addEventListener("click", mostrar_menu);
+document.getElementById("btn-menu").addEventListener("click", show_menu);
 
-function mostrar_menu(){
-    document.querySelector(".menu").classList.toggle("mostrar_menu")
+function show_menu(){
+    document.querySelector(".menu").classList.toggle("show_menu");
 }
